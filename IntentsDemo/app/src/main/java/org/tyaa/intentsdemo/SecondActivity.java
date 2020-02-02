8ce60f9d68db55d0126ab28427d7931caf04b3ea
YuriiTrofimenko <tyaa@ukr.net>
2020-02-02 15:28:08 +0200

    init n setup

---
+++ IntentsDemo/app/src/main/java/org/tyaa/intentsdemo/SecondActivity.java
@@ -0,0 +1,14 @@
+package org.tyaa.intentsdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
