4e644090c30cc12a0d3bf5303f4382647b77ab42
YuriiTrofimenko <tyaa@ukr.net>
2020-02-02 15:40:52 +0200

    send string data from parent activity to child

--- IntentsDemo/app/src/main/java/org/tyaa/intentsdemo/MainActivity.java
+++ IntentsDemo/app/src/main/java/org/tyaa/intentsdemo/MainActivity.java
@@ -4,22 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String userName = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button button = findViewById(R.id.clickMeButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent =
                         new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("userName", userName);
                 startActivity(intent);
+            }
+        });
+
+        EditText nameEditText =
+                findViewById(R.id.nameEditText);
+        nameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                userName = s.toString();
             }
         });
     }
--- IntentsDemo/app/src/main/java/org/tyaa/intentsdemo/SecondActivity.java
+++ IntentsDemo/app/src/main/java/org/tyaa/intentsdemo/SecondActivity.java
@@ -3,6 +3,7 @@ package org.tyaa.intentsdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        String userName =
+            getIntent().getStringExtra("userName");
+        if (userName != null) {
+            Toast.makeText(this, "Hello, " + userName, Toast.LENGTH_LONG).show();
+        }
     }
 }
